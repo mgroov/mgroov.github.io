15746bf7617815dee1bc2c93b5604e9a894903b6
M.Groov <mjgrooverelp@gmail.com>
2022-08-21 17:04:52 -0600

    Basic Works
    not that great

---
+++ starstruck/scripts/index.js
@@ -0,0 +1,12 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d'); //gets relation for drawing
+
+
+canvas.width = 854;
+canvas.height  = 480;
+
+c.fillStyle = 'grey'
+c.fillRect(0,0,canvas.width,canvas.height)//highlight the canvas''
+
+
+// b1.addEventListener("click",b1click());
