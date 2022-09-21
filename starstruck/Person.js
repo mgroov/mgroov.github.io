a1322ffc16cb9a3beb26c5398b434729b649cc31
M.Groov <mjgrooverelp@gmail.com>
2022-09-21 16:33:32 -0600

    adw

--- starstruck/Person.js
+++ starstruck/Person.js
@@ -56,7 +56,7 @@ class Person extends GameObject{
 
             //ready to walk
             state.map.movewall(this.x,this.y,this.direction);
-            this.movingProgressRemaining =16;
+            this.movingProgressRemaining =8;
             this.updateSprite(state);
         }
 
